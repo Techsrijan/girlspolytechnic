b9704101aebc28cdb3c8691146f8bc9ed187be6a
Ashwani <aswanibtech@gmail.com>
2020-10-27 11:35:39 +0530

    while loop

---
+++ evenodd.py
@@ -0,0 +1,27 @@
+n=61
+if n%2==0:
+    print("\n even No")
+else:
+    print("\n Odd no")
+''' 
+i=1
+while i<=100:
+    if i % 2 == 0:
+        print(i,"=even No",end="")
+    else:
+        print(i,"=Odd no",end="")
+    i=i+1
+    
+'''
+
+x=int(input("enter first no"))
+y=int(input("Enter Second No"))
+
+
+i=x
+while i<=y:
+    if i % 2 == 0:
+        print(i,"=even No",end="")
+    else:
+        print(i,"=Odd no",end="")
+    i=i+1
---
+++ loopexp.py
@@ -0,0 +1,22 @@
+'''i=1   #initialization
+while i<=10:    #condition
+    print("i=",i,"Happy b-day")  #statement
+    i=i+1 #updation
+
+
+i=1   #initialization
+while i>=10:    #condition
+    print("i=",i,"Happy b-day")  #statement
+    i=i+1 #updation
+    '''
+
+i=2   #initialization
+while i<=20:    #condition
+    print("i=",i,"Happy b-day")  #statement
+    i=i+2 #updation
+
+n=int(input("Enter Any No" ))
+i=1   #initialization
+while i<=10:    #condition
+    print(n,"*",i,"=",i*n) #statement
+    i=i+1 #updation
